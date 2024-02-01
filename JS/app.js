1c948a677855e7261a2005b41a6a933930e0d6a2
Zana Amanallhy <zanaamanallhy80@gmail.com>
2024-02-01 15:08:38 +0330

    Making edit, do, delete buttons dynamic. And finally, I fixed the tasks to make it easier for the user.

--- JS/app.js
+++ JS/app.js
@@ -1,12 +1,14 @@
 const taskInp = document.getElementById("task-input");
 const dateInp = document.getElementById("date-input");
 const addBtn = document.getElementById("add-btn");
+const editBtn = document.getElementById("edit-btn");
 const todosBody = document.querySelector("tbody");
 const alertSection = document.querySelector(".alert-message");
 const deleteAllButton = document.getElementById("delete-all");
+const filterBtn = document.querySelectorAll(".filter-todo");
 
+let todos = JSON.parse(localStorage.getItem('todos')) || [];
 
-let todos = JSON.parse(localStorage.getItem("todos")) || [];
 
 const saveToLocalStorage = () => {
     localStorage.setItem("todos", JSON.stringify(todos))
@@ -30,23 +32,24 @@ const showAlert = (message, type) => {
     }, 2500);
 };
 
-const displayTodos = () => {
-    
+const displayTodos = data => {
+    const newTodos = data || todos
+
     todosBody.innerHTML = "";
-    if (!todos.length) {
+    if (!newTodos.length) {
         todosBody.innerHTML = "<tr><td colspan='4'>No task found</td></tr>";
     }
 
-    todos.forEach(todo => {
+    newTodos.forEach(todo => {
         todosBody.innerHTML += `
             <tr>
                 <td>${todo.task}</td>
                 <td>${todo.date ? todo.date : "No Date"}</td>
                 <td>${todo.completed ? "Completed" : "Pending"}</td>
                 <td>
-                    <button onClick="editHandler(${todo.id})">Edit</button>
-                    <button onClick="toggleHandler(${todo.id})">Do</button>
-                    <button onClick="deleteHandler(${todo.id})">Delete</button>
+                    <button onClick="editHandler('${todo.id}')">Edit</button>
+                    <button onClick="toggleHandler('${todo.id}')">${todo.completed ? "Undo" : "Do"}</button>
+                    <button onClick="deleteHandler('${todo.id}')">Delete</button>
                 </td>
             </tr>
         `
@@ -89,10 +92,65 @@ const deleteAllHandler = () => {
 
 const editHandler = id => {
     const todo = todos.find(todo => todo.id === id);
-    console.log(todo);
-    // taskInp.value = 
+    taskInp.value = todo.task;
+    dateInp.value = todo.date;
+    addBtn.style.display = "none";
+    editBtn.style.display = "inline-block";
+    editBtn.dataset.id = id;
+};
+
+const applyEditHandler = event => {
+    const id = event.target.dataset.id;
+    const todo = todos.find(todo => todo.id === id);
+
+    todo.task = taskInp.value;
+    todo.date = dateInp.value;
+    addBtn.style.display = "inline-block";
+    editBtn.style.display = "none";
+    saveToLocalStorage();
+    displayTodos();
+    showAlert("Todo edited successfuly", "success");
+};
+
+const toggleHandler = id => {
+    const todo = todos.find(todo => todo.id === id);
+    todo.completed = !todo.completed;
+    saveToLocalStorage();
+    displayTodos();
+    showAlert('Todo status changed successfuly', 'success');
+};
+
+const deleteHandler = id => {
+    const newTodos = todos.filter(todo => todo.id !== id);
+    todos = newTodos;
+    saveToLocalStorage();
+    displayTodos();
+    showAlert('Todo deleted successfuly', 'success')
+};
+
+const filterHandler = event => {
+    let filteredTodo = null;
+    const filter = event.target.dataset.filter;
+    switch (filter) {
+        case "pending":
+            filteredTodo = todos.filter(todo => todo.completed === false);
+            break;
+        case "completed":
+            filteredTodo = todos.filter(todo => todo.completed === true);
+            break;
+
+        default:
+            filteredTodo = todos;
+            break;
+    }
+
+    displayTodos(filteredTodo);
 }
 
-window.addEventListener("load", displayTodos)
+window.addEventListener("load", () => displayTodos());
 addBtn.addEventListener("click", addButtonHandler);
+editBtn.addEventListener("click", applyEditHandler);
 deleteAllButton.addEventListener("click", deleteAllHandler);
+filterBtn.forEach(btn => {
+    btn.addEventListener("click", filterHandler)
+})
